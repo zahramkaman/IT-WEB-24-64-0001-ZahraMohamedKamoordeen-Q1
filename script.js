cc3863c5478ddd5ca0e43c39f95ca751c5070815
ZAHRA MOHAMED KAMOORDEEN <zahra96mohd@gmail.com>
2024-10-13 22:13:56 +0400

    updated modal

--- script.js
+++ script.js
@@ -37,3 +37,55 @@ function emailId2 () {
         submit = true;
     }
 };
+
+// modal form validation
+
+const submitBtn = document.querySelector("#submitBtn");
+
+submitBtn.addEventListener('click', submitForm);
+
+function submitForm () {
+
+    const firstName = document.querySelector("#firstName").value;
+    const lastName = document.querySelector("#lastName").value;
+    const email = document.querySelector("#email").value;
+    const number = document.querySelector("#number").value;
+
+    if (firstName === "") {
+        const error1 = document.querySelector("#error1")
+        error1.innerHTML = "Please enter your name";
+        submit = false;
+        console.log("work");
+    } else {
+        error1.innerHTML = "";
+        submit = true;
+    }
+
+    if (lastName === "") {
+        const error2 = document.querySelector("#error2")
+        error2.innerHTML = "Please enter your name";
+        submit = false;
+    } else {
+        error2.innerHTML = "";
+        submit = true;
+    }
+
+    if (email === "") {
+        const error3 = document.querySelector("#error3")
+        error3.innerHTML = "Please enter a valid email";
+        submit = false;
+    } else {
+        error3.innerHTML = "";
+        submit = true;
+    }  
+
+    if (number === "") {
+        const error4 = document.querySelector("#error4")
+        error4.innerHTML = "Please enter your number";
+        submit = false;
+    } else {
+        error4.innerHTML = "";
+        submit = true;
+    }  
+
+}
